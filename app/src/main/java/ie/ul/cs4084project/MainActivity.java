d41d5c748c0c6b244f2af129917aab3202dc7dbd
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2020-10-31 20:37:53 +0000

    Added basic email and password sign-in with firebase.

--- app/src/main/java/ie/ul/cs4084project/MainActivity.java
+++ app/src/main/java/ie/ul/cs4084project/MainActivity.java
@@ -2,13 +2,59 @@ package ie.ul.cs4084project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSignIn(View view) {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build() , RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode, data);
+
+        if(requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if(resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                System.out.println( "sign in successful\n" + "name ="
+                        + user.getDisplayName() + "\nemail = " + user.getEmail() +
+                        "\nid = " + user.getUid() );
+            } else {
+                if (response == null) {
+                    System.out.println("sign in cancelled"); return;
+                }
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    System.out.println("no internet connection"); return;
+                }
+            }
+        }
     }
 }
