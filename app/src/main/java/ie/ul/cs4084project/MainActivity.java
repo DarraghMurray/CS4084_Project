91dbd6b8548321cdfed00e5a7f7d36eb744aa213
Nathan Quirke <18248195@studentmail.ul.ie>
2020-11-06 19:24:17 +0000

    Unable to add google maps 06/11/20, this is just a revert back to master on my branch

--- app/src/main/java/ie/ul/cs4084project/MainActivity.java
+++ app/src/main/java/ie/ul/cs4084project/MainActivity.java
@@ -1,22 +1,7 @@
 package ie.ul.cs4084project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.app.FragmentManager;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,20 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onHomeClicked(View view){
-        MainFeed newFragment = new MainFeed();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment, newFragment);
-        ft.commit();
-    }
-
-    public void onCreatePostClicked(View view) {
-        CreatePost newFragment = new CreatePost();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment, newFragment);
-        ft.commit();
     }
 
 }
