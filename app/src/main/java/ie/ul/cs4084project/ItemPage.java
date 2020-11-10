d9272dfb3a6a6e746d2ad3102f17be366b9c25ad
Nathan Quirke <18248195@studentmail.ul.ie>
2020-11-10 20:11:34 +0000

    Map function should be added but there is a problem with the view as it crashes when opening ItemPage.java

--- app/src/main/java/ie/ul/cs4084project/ItemPage.java
+++ app/src/main/java/ie/ul/cs4084project/ItemPage.java
@@ -15,11 +15,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.w3c.dom.Text;
+
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,9 +35,7 @@ import org.w3c.dom.Text;
  */
 public class ItemPage extends Fragment implements OnMapReadyCallback {
 
-    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
     public MapView mMapView;
-    private static final String TAG = "UserListFragment";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -45,6 +50,8 @@ public class ItemPage extends Fragment implements OnMapReadyCallback {
     private TextView itemDescrip;
     private TextView itemPricing;
     private TextView itemSign;
+    GoogleMap mGoogleMap;
+    View mview;
 
     public ItemPage() {
         // Required empty public constructor
@@ -80,16 +87,8 @@ public class ItemPage extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_item_page, container, false);
-        mMapView = (MapView) view.findViewById(R.id.user_list_map);
-        Bundle mapViewBundle = null;
-        if (savedInstanceState != null) {
-            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
-        }
-
-        mMapView.onCreate(mapViewBundle);
-        mMapView.getMapAsync(this);
-        return view;
+        mview = inflater.inflate(R.layout.fragment_item_page, container, false);
+        return mview;
     }
 
     @Override
@@ -106,72 +105,22 @@ public class ItemPage extends Fragment implements OnMapReadyCallback {
         itemDescrip.setText(getArguments().getString("ItemDescription"));
         itemPricing.setText(Double.toString(getArguments().getDouble("ItemPrice")));
         itemSign.setText("€");
-    }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
-        if (mapViewBundle == null) {
-            mapViewBundle = new Bundle();
-            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        mMapView = (MapView) mview.findViewById(R.id.user_list_map);
+        if(mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(null);
         }
-
-        mMapView.onSaveInstanceState(mapViewBundle);
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        mMapView.onResume();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mMapView.onStart();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mMapView.onStop();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        map.setMyLocationEnabled(true);
-    }
-
-    @Override
-    public void onPause() {
-        mMapView.onPause();
-        super.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        mMapView.onDestroy();
-        super.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mMapView.onLowMemory();
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.673872, -8.575679)).title("Limerick"));
+        CameraPosition Limerick = CameraPosition.builder().target(new LatLng(52.673872, -8.575679)).zoom(16).bearing(0).tilt(50).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Limerick));
     }
 }
