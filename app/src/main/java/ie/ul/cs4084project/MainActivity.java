1b77f0114b2faf19e67a8948854fc4cb993165ff
Nathan Quirke <18248195@studentmail.ul.ie>
2020-11-10 22:29:57 +0000

    match main master

--- app/src/main/java/ie/ul/cs4084project/MainActivity.java
+++ app/src/main/java/ie/ul/cs4084project/MainActivity.java
@@ -1,27 +1,16 @@
 package ie.ul.cs4084project;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Spinner;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private TextInputLayout textInputSearch;
+    private Spinner categorySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onHomeClicked(View view){
-
         MainFeed newFragment = new MainFeed();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment, newFragment);
@@ -42,5 +30,19 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment, newFragment);
         ft.commit();
+    }
+
+    public void onSearchClicked(View view) {
+        FindItem newFragment = new FindItem();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        Bundle args = new Bundle();
+        args.putString("searchTxt", textInputSearch.getEditText().getText().toString());
+        args.putString("Category", categorySpinner.getSelectedItem().toString());
+        newFragment.setArguments(args);
+
+        ft.replace(R.id.fragment, newFragment);
+        ft.commit();
+
     }
 }
