ca9391960d88bf11186bf3ac5f52937e1954b6b6
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2020-11-28 00:16:24 +0000

    Merge branch 'Darragh' of C:\Users\DMurray\AndroidStudioProjects\CS4084Project with conflicts.

--- app/src/main/java/ie/ul/cs4084project/CreatePost.java
+++ app/src/main/java/ie/ul/cs4084project/CreatePost.java
@@ -1,14 +1,17 @@
 package ie.ul.cs4084project;
 
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
