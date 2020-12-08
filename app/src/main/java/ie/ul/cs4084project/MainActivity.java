17d5787da9e6c69a2c45dc36e01c51973e1b5077
Nathan Quirke <18248195@studentmail.ul.ie>
2020-12-08 14:17:53 +0000

    Fixes to layouts and colours

--- app/src/main/java/ie/ul/cs4084project/MainActivity.java
+++ app/src/main/java/ie/ul/cs4084project/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.textInputSearch);
+
         categorySpinner = findViewById(R.id.categorySpinner);
     }
 
